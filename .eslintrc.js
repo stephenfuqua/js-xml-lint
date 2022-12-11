57d93ff923ba2e18ecb4bd991313d10af7d6a5e5
Stephen A. Fuqua <stephen@safnet.com>
2022-12-11 11:58:59 -0600

    Remove xo Use airbnb style Prepare for CLI development

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: ['airbnb-base', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier'],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.mjs', '.js', '.json', '.ts'],
+        },
+      },
+      'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+    },
+    parserOptions: {
+      project: ['./tsconfig.eslint.json'],
+      tsconfigRootDir: __dirname,
+    },
+    rules: {
+      // Let TypeScript compiler handle this
+      'no-unused-vars': 'off',
+
+      // Catch common async/await problems
+      '@typescript-eslint/no-misused-promises': 2,
+      '@typescript-eslint/no-floating-promises': [ 2, { ignoreIIFE: true } ],
+      '@typescript-eslint/promise-function-async': 2,
+
+      // originally required in the early days of monorepos -- consider revisiting
+      'import/no-cycle': 'off',
+
+      // need to dynamically load plugins
+      'import/no-dynamic-require': 'off',
+
+      // prettier is handling this
+      'max-len': 'off',
+
+      // our loop iterations are rarely independent, and it's a more readable syntax
+      'no-await-in-loop': 'off',
+
+      // we almost never do default exports
+      'import/prefer-default-export': 'off',
+
+      // relax this to avoid unnecessary temp variables
+      'no-param-reassign': [ 2, { props: false } ],
+
+      // prettier issues are warnings here
+      'prettier/prettier': 'warn',
+
+      // not sure why this is necessary, became so after upgrading
+      'import/extensions': 0,
+    },
+  };
